bed735276fba365c8c20a3f329d7740731d56b9d
Foufou-exe <thibaut.maurras34@gmail.com>
2024-01-05 16:48:18 -0500

    ✨ feat(ADD COURS FORMATEUR): TRAINING C++
    ADD FILES

---
+++ test/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+
+
+// La fonction à pour but de donner la taille des types de données
+// int main()
+// {
+//     cout << "sizeof information" << endl;
+//     cout << "========================" << endl;
+//     cout << "char: " << sizeof(char) << " bytes" << endl;
+//     cout << "int: " << sizeof(int) << " bytes" << endl;
+//     cout << "unsigned int: " << sizeof(unsigned int) << " bytes" << endl;
+//     cout << "short: " << sizeof(short) << " bytes" << endl;
+//     cout << "long: " << sizeof(long) << " bytes" << endl;
+//     cout << "long long: " << sizeof(long long) << " bytes" << endl;
+//     cout << "========================" << endl;
+
+// }
+
+
+// La fonction a pour but de compter de 0 à 100
+// int main()
+// {
+//     int number {0};
+
+//     for (int i = 0; i < 100; i++)
+//     {
+//         number += 1;
+//         std::cout << number << std::endl;
+//     }
+//     return 0;
+// } 
